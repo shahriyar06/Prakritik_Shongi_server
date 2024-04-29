3a89ce87bba1b19f6cb0b99c54c62542a7c2b2d6
Shahriyar <chyshahriyar@gmail.com>
2024-04-29 12:05:08 +0600

    Get single data by id from my database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -33,17 +33,32 @@ async function run() {
 
     const craftcollection = client.db('craftDB').collection('craft');
 
-    app.get('/craftlist', async(req, res) =>{
+    app.get('/craftlist', async (req, res) => {
       const cursor = craftcollection.find();
       const result = await cursor.toArray();
       res.send(result);
-  })
-
-    app.post('/craftlist', async(req, res) =>{
-        const newcraft = req.body;
-        const result = await craftcollection.insertOne(newcraft);
-        res.send(result);
     })
+
+    // app.get('/craftlist/:id', async (req, res) => {
+    //   const id = craftcollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // })
+    app.get('/craftlist/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftcollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/craftlist', async (req, res) => {
+      const newcraft = req.body;
+      const result = await craftcollection.insertOne(newcraft);
+      res.send(result);
+    })
+
+
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -57,9 +72,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Prakritik Shongi-server is running...')
+  res.send('Prakritik Shongi-server is running...')
 })
 
 app.listen(port, () => {
-    console.log(`Prakritik Shongi-server is running on port : ${port}`)
+  console.log(`Prakritik Shongi-server is running on port : ${port}`)
 })
