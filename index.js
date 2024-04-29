21be69291f19431129a68c3b91633a08daf3d7a4
Shahriyar <chyshahriyar@gmail.com>
2024-04-29 12:37:20 +0600

    user can delete data for his own page

--- index.js
+++ index.js
@@ -58,7 +58,12 @@ async function run() {
     })
 
 
-    
+    app.delete('/craftlist/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftcollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
